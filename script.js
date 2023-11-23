0857966f484a193b40a7584337a78d04cd5be014
to-yoko <151840472+to-yoko@users.noreply.github.com>
2023-11-23 21:06:55 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var inp, aud;
+var pl = [];
+onload = () => {
+  navigator.serviceWorker.register('sw.js');
+  inp = document.querySelector('input');
+  aud = document.querySelctor('audio');
+  inp.onchange = (e) => {
+    for(let i=0; i < e.target.files.length; i++) {
+      pl.push(URL.createObjectURL(e.target.files[i]));
+    }
+  }
+}
